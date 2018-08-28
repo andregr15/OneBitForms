e340fccff6cdfb560f4a271624457a99d986c6e3
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-08-28 07:32:53 -0300

    Setting up devise_token_auth and test environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
