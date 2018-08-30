fd48b4a6a7297232334b66e66404f70993065306
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-08-30 13:59:05 -0300

    Adding ActiveRecord::RecordNotFound and ActionController:ParameterMissing Exceptions handling

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,17 @@
 module Api::V1
-  class ApplicationController < ActionController
+  class ApiController < ApplicationController
     # globals methods
-    
+    rescue_from(ActiveRecord::RecordNotFound) do
+      render(json: { message: 'Not Found' }, status: :not_found)
+    end
+
+    rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+      render(
+        json: { 
+            message: { "Required parameter is missing: #{parameter_missing_exception.param}" }, 
+            status: :bad_request
+          }
+        )
+    end
   end
 end
